c7df0bbcc2343b20902065d0ece988ea04228e1f
Ourten <ourtencoop@gmail.com>
2019-04-15 22:01:27 +0200

    Update test render to use textures

--- src/main/java/net/vi/ashrunica/client/render/RuneTestRender.java
+++ src/main/java/net/vi/ashrunica/client/render/RuneTestRender.java
@@ -11,6 +11,11 @@ import net.voxelindustry.brokkgui.paint.Color;
 
 public class RuneTestRender extends TileEntitySpecialRenderer<TileRuneTest>
 {
+    private final ResourceLocation glyphAtlas = new ResourceLocation(AshRunica.MODID, "textures/runes/runes_atlas.png");
+    private final ResourceLocation ring48     = new ResourceLocation(AshRunica.MODID, "textures/runes/ring_48.png");
+    private final ResourceLocation ring12     = new ResourceLocation(AshRunica.MODID, "textures/runes/ring_12.png");
+    private final ResourceLocation ring8      = new ResourceLocation(AshRunica.MODID, "textures/runes/ring_8.png");
+
     @Override
     public void render(TileRuneTest te, double x, double y, double z, float partialTicks, int destroyStage, float alpha)
     {
@@ -27,48 +32,15 @@ public class RuneTestRender extends TileEntitySpecialRenderer<TileRuneTest>
         OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, (float) i1, (float) j1);
 
         long modulo = te.getWorld().getTotalWorldTime() % 360;
-        GlStateManager.rotate(modulo + 1 * partialTicks, 0, 0, 1);
         GlStateManager.scale(0.0625f / 4, 0.0625f / 4, 0.0625f / 4);
 
-        // Ext ring
-        RenderHelper.drawColoredCircle(Tessellator.getInstance(), 0, 0, 48, 0, Color.BLACK);
-        RenderHelper.drawColoredCircle(Tessellator.getInstance(), 0, 0, 47, 0, Color.LIGHT_GRAY);
-        RenderHelper.drawColoredCircle(Tessellator.getInstance(), 0, 0, 46, 0, Color.BLACK);
+        this.bindTexture(ring12);
+        RenderHelper.drawTexturedRect(Tessellator.getInstance(), -12, -12, 24, 24, 0, 0, 0, 1, 1);
 
-        // Main ring
-        RenderHelper.drawColoredCircle(Tessellator.getInstance(), 0, 0, 12, 0, Color.BLACK);
-        RenderHelper.drawColoredCircle(Tessellator.getInstance(), 0, 0, 11, 0, Color.LIGHT_GRAY);
-        RenderHelper.drawColoredCircle(Tessellator.getInstance(), 0, 0, 10, 0, Color.BLACK);
+        this.bindTexture(glyphAtlas);
+        RenderHelper.drawTexturedRect(Tessellator.getInstance(), -6, -6, 12, 12, 0, 16 / 24f, 0, 17 / 24f, 1);
 
-        // Int Ring 1
-        RenderHelper.drawColoredCircle(Tessellator.getInstance(), 20, 20, 8, 0, Color.BLACK);
-        RenderHelper.drawColoredCircle(Tessellator.getInstance(), 20, 20, 7, 0, Color.LIGHT_GRAY);
-        RenderHelper.drawColoredCircle(Tessellator.getInstance(), 20, 20, 6, 0, Color.BLACK);
-
-        // Int Ring 2
-        RenderHelper.drawColoredCircle(Tessellator.getInstance(), -20, -20, 8, 0, Color.BLACK);
-        RenderHelper.drawColoredCircle(Tessellator.getInstance(), -20, -20, 7, 0, Color.LIGHT_GRAY);
-        RenderHelper.drawColoredCircle(Tessellator.getInstance(), -20, -20, 6, 0, Color.BLACK);
-
-        // Periph Ring 1
-        RenderHelper.drawColoredCircle(Tessellator.getInstance(), 0, 46, 8, 0, Color.BLACK);
-        RenderHelper.drawColoredCircle(Tessellator.getInstance(), 0, 46, 7, 0, Color.LIGHT_GRAY);
-        RenderHelper.drawColoredCircle(Tessellator.getInstance(), 0, 46, 6, 0, Color.BLACK);
-
-        // Periph Ring 2
-        RenderHelper.drawColoredCircle(Tessellator.getInstance(), 0, -46, 8, 0, Color.BLACK);
-        RenderHelper.drawColoredCircle(Tessellator.getInstance(), 0, -46, 7, 0, Color.LIGHT_GRAY);
-        RenderHelper.drawColoredCircle(Tessellator.getInstance(), 0, -46, 6, 0, Color.BLACK);
-
-        RenderHelper.drawColoredRect(Tessellator.getInstance(), 12, -3, 34, 1, 0, Color.BLACK);
-        RenderHelper.drawColoredRect(Tessellator.getInstance(), 12, 3, 34, 1, 0, Color.BLACK);
-
-        RenderHelper.drawColoredRect(Tessellator.getInstance(), -12 - 34, -3, 34, 1, 0, Color.BLACK);
-        RenderHelper.drawColoredRect(Tessellator.getInstance(), -12 - 34, 3, 34, 1, 0, Color.BLACK);
-
-        this.bindTexture(new ResourceLocation(AshRunica.MODID, "textures/runes/runes_atlas.png"));
-
-        GlStateManager.rotate(90, 0, 0, 1);
+        GlStateManager.rotate(modulo + 1 * partialTicks, 0, 0, 1);
 
         for (int i = 0; i < 7; i++)
             RenderHelper.drawTexturedRect(Tessellator.getInstance(), -1.5f, -17 - 4.5f * i, 4, 4, 0,
@@ -78,7 +50,41 @@ public class RuneTestRender extends TileEntitySpecialRenderer<TileRuneTest>
             RenderHelper.drawTexturedRect(Tessellator.getInstance(), -1.5f, 13 + 4.5f * i, 4, 4, 0,
                     (i + 8) * (1 / 24f), 0, (i + 9) * (1 / 24f), 1);
 
-        RenderHelper.drawTexturedRect(Tessellator.getInstance(), -6, -6, 12, 12, 0, 16/24f, 0, 17 / 24f, 1);
+        this.bindTexture(ring48);
+        RenderHelper.drawTexturedRect(Tessellator.getInstance(), -48, -48, 96, 96, 0, 0, 0, 1, 1);
+
+        RenderHelper.drawColoredRect(Tessellator.getInstance(), -3, 11, 1, 33, 0, Color.BLACK);
+        RenderHelper.drawColoredRect(Tessellator.getInstance(), 3, 11, 1, 33, 0, Color.BLACK);
+
+        RenderHelper.drawColoredRect(Tessellator.getInstance(), -3, -11 - 33, 1, 33, 0, Color.BLACK);
+        RenderHelper.drawColoredRect(Tessellator.getInstance(), 3, -11 - 33, 1, 33, 0, Color.BLACK);
+        GlStateManager.color(1, 1, 1, 1);
+
+        this.bindTexture(ring8);
+
+        GlStateManager.pushMatrix();
+        GlStateManager.translate(-20, -20, 0);
+        GlStateManager.rotate(-(modulo + 1 * partialTicks), 0, 0, 1);
+        RenderHelper.drawTexturedRect(Tessellator.getInstance(), -8, -8, 16, 16, 4, 0, 0, 1, 1);
+        GlStateManager.popMatrix();
+
+        GlStateManager.pushMatrix();
+        GlStateManager.translate(20, 20, 0);
+        GlStateManager.rotate(-(modulo + 1 * partialTicks), 0, 0, 1);
+        RenderHelper.drawTexturedRect(Tessellator.getInstance(), -8, -8, 16, 16, 4, 0, 0, 1, 1);
+        GlStateManager.popMatrix();
+
+        GlStateManager.pushMatrix();
+        GlStateManager.translate(0, -44, 0);
+        GlStateManager.rotate(-(modulo + 1 * partialTicks), 0, 0, 1);
+        RenderHelper.drawTexturedRect(Tessellator.getInstance(), -8, -8, 16, 16, -4, 0, 0, 1, 1);
+        GlStateManager.popMatrix();
+
+        GlStateManager.pushMatrix();
+        GlStateManager.translate(0, 44, 0);
+        GlStateManager.rotate(-(modulo + 1 * partialTicks), 0, 0, 1);
+        RenderHelper.drawTexturedRect(Tessellator.getInstance(), -8, -8, 16, 16, -4, 0, 0, 1, 1);
+        GlStateManager.popMatrix();
 
         GlStateManager.popMatrix();
     }
